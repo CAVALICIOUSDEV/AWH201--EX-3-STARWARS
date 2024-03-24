9eab656bdf068bbc29706818e9ceda6ec11722ff
CavTG <ace.200112@gmail.com>
2024-03-24 21:11:02 +0100

    Starwars Project Examination AWH110

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/Users/cavtop/Desktop/Skola/kapitel-3/AWH201--EX-3-STARWARS/public/{index.html,jedi.html,sith.html,swtor.html,kotor.html}"],
+  theme: {
+    colors: {
+      "black": "#080808",
+      "gray": "#3b444b",
+      "vit": "#f0ece1",
+      "red": "#dd4e1e",
+      "orange": "#e5703d",
+      "blue": "#2E67F8"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
